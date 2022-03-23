2bf61d86a0ac936260568c75c5445ac67dc95820
DingusDad <the.chris.parisi@gmail.com>
2022-03-22 19:48:38 -0700

    MotionWrapper added

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             <span>👋</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, I am</p>
-              <h1 className="header-text">Chris</h1>
+              <h1 className="header-text">Chris Parisi</h1>
             </div>
           </div>
 
