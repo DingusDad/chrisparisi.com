cf500dbec3886ead005c5d6db3b33723d7c74e9d
DingusDad <the.chris.parisi@gmail.com>
2022-05-06 12:53:56 -0700

    header v2

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,63 +1,32 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React from 'react';
+import { motion } from 'framer-motion';
 
-import { AppWrap } from "../../wrapper";
-import { images } from "../../constants";
-import "./Header.scss";
-
-const scaleVariants = {
-  whileInView: {
-    scale: [0, 1],
-    opacity: [0, 1],
-    transition: {
-      duration: 1,
-      ease: "easeInOut",
-    },
-  },
-};
+import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
+import './Header.scss';
 
 const Header = () => {
   return (
     <div id="home" className="app__header app__flex">
       <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
+        whileInView={{ x: [-400, 0], opacity: [0, 1] }}
+        transition={{ duration: 2 }}
         className="app__header-info"
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello, I am</p>
-              <h1 className="header-text">Chris Parisi</h1>
+            <div>
+              <h1>CHRIS PARISI</h1>
+              <h3>Fullstack Developer</h3>
+              <h3>/MongoDB - Express - ReactJS - NodeJS</h3>
             </div>
-          </div>
-
-          <div className="tag-cmp app__flex">
-            <p className="p-text">Fullstack Developer</p>
-            <p className="p-text">/ MongoDB - Express - ReactJS - NodeJS</p>
           </div>
         </div>
       </motion.div>
 
       <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.profile} alt="profile_bg" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          className="overlay_circle"
-          src={images.circle}
-          alt="profile_circle"
-        />
-      </motion.div>
-
-      <motion.div
-        variant={scaleVariants}
-        whileInView={scaleVariants.whileInView}
+        whileInView={{ x: [400, 0], opacity: [0, 1] }}
+        transition={{ duration: 2 }}
         className="app__header-circles"
       >
         {[images.node, images.react1, images.redux].map((circle, index) => (
@@ -70,4 +39,4 @@ const Header = () => {
   );
 };
 
-export default AppWrap(Header, "home");
+export default AppWrap(Header, 'home');
