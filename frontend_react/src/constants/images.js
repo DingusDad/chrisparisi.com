633acbf85ad3f903a5a3472206cb9cd9e458adf0
DingusDad <the.chris.parisi@gmail.com>
2022-10-07 11:12:54 -0700

    removed redundant files

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,4 +1,3 @@
-import email from '../assets/email.png';
 import email1 from '../assets/email1.png';
 import smartphone from '../assets/smartphone.png';
 import mobile from '../assets/mobile.png';
@@ -6,7 +5,6 @@ import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
@@ -20,34 +18,17 @@ import react1 from '../assets/react1.png';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
-
-import circle from '../assets/circle.svg';
 import chris from '../assets/chris.png';
-
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
 
 export default {
   email1,
   smartphone,
-  email,
   mobile,
   api,
   cpp,
   css,
   figma,
-  flutter,
   git,
   graphql,
   html,
@@ -60,18 +41,5 @@ export default {
   redux,
   sass,
   typescript,
-  vue,
-  about01,
-  about02,
-  about03,
-  about04,
-  circle,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
   chris,
 };
